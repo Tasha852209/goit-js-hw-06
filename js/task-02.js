0a5fa83f01173e58043b8b45083e83f27b76c6a2
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-07-14 15:26:41 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ulIngredients = document.querySelector(`#ingredients`);
+
+const createEls = (ingredients) => {
+  return ingredients.map((el) => {
+    const ingredient = document.createElement("li");
+    ingredient.textContent = el;
+    ingredient.classList.add("item");
+    return ingredient;
+  });
+};
+const items = createEls(ingredients);
+ulIngredients.append(...items);
