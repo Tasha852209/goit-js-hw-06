e4392e4d7e1ebf65c4dc4565a4f6781868f626eb
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-07-16 22:46:21 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const bodyToChange = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color");
+const spanText = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", onBodyColor);
+
+function onBodyColor() {
+  bodyToChange.style.backgroundColor = getRandomHexColor();
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  spanText.textContent = `${getRandomHexColor()}`;
 }
