6d0671531139ae3f8607f8a9f9590c668060de52
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-07-19 15:37:18 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,35 @@
+const input = document.querySelector("input");
+const boxes = document.getElementById("boxes");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+const newElements = [];
+
+function createBoxes() {
+  const amount = input.value;
+
+  let widthPerBox = 30;
+
+  for (let i = 1; i <= amount; i += 1) {
+    widthPerBox += 10;
+    const createEl = document.createElement("div");
+    createEl.style.backgroundColor = getRandomHexColor();
+    createEl.style.width = `${widthPerBox}px`;
+    createEl.style.height = `${widthPerBox}px`;
+    newElements.push(createEl);
+  }
+  boxes.append(...newElements);
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function destroyBoxes() {
+  input.value = "";
+  boxes.remove([...newElements]);
 }
